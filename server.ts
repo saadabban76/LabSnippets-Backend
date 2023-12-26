6f12dba4768a0f9a57d946c7ea94c6e77ff36f38
saadabban76 <saadabban76@gmail.com>
2023-12-26 20:37:55 +0530

    added env file

--- server.ts
+++ server.ts
@@ -57,7 +57,9 @@ io.on('connection', (socket: any) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
+console.log("port : ", PORT);
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
